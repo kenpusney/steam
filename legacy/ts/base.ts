2561a0e1d5e69f36c3c73e5ae66972bea37d8f70
KimmyLeo <kenpusney@gmail.com>
2021-03-02 18:06:27 +0800

    remove legacy

--- legacy/ts/base.ts
+++
@@ -1,9 +0,0 @@
-export function extend(self: any, obj: any) {
-    for (var i in obj) {
-        if (obj.hasOwnProperty(i)) {
-            self[i] = obj[i];
-        }
-    }
-}
-
-export function identity<T>(self: T): T {return self;}
