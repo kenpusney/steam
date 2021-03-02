529e957d91893e81ba59e1113d1cf425cad8a9d7
KimmyLeo <kenpusney@gmail.com>
2021-03-02 21:17:50 +0800

    update

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /.less$/,
+        use: [{
+          loader: "style-loader",
+        }, {
+          loader: "css-loader",
+        }, {
+          loader: "less-loader",
+        },],
+      },
+      {
+        test: /.css$/,
+        use: [{
+          loader: "style-loader",
+        }, {
+          loader: "css-loader",
+        },],
+      },
+    ]
+  }
+}
