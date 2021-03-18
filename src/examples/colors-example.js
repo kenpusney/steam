dcb4fc7a3a8c86b9ffbb03eb2d953016cb0e3e6b
KimmyLeo <kenpusney@gmail.com>
2021-03-18 16:12:36 +0800

    reformat code

--- src/examples/colors-example.js
+++ src/examples/colors-example.js
@@ -1,35 +1,35 @@
 import {append, html} from "../util/element";
 
 append(html`
-<div class="colors-examole">
-  <div class="demo-buttons">
-    <button class="element-primary">Primary Button</button>
-    <button class="element-success">Success Button</button>
-    <button class="element-warning">Warning Button</button>
-    <button class="element-danger">Danger Button</button>
-    <button class="element-secondary">Secondary Button</button>
+  <div class="colors-examole">
+    <div class="demo-buttons">
+      <button class="element-primary">Primary Button</button>
+      <button class="element-success">Success Button</button>
+      <button class="element-warning">Warning Button</button>
+      <button class="element-danger">Danger Button</button>
+      <button class="element-secondary">Secondary Button</button>
+    </div>
+
+    <div class="demo-buttons">
+      <h3>disabled:</h3>
+      <button class="element-primary" disabled>Primary Button</button>
+      <button class="element-success" disabled>Success Button</button>
+      <button class="element-warning" disabled>Warning Button</button>
+      <button class="element-danger" disabled>Danger Button</button>
+      <button class="element-secondary" disabled>Secondary Button</button>
+    </div>
+
+    <style type="text/css">
+      button {
+        padding: 1em;
+        border: none;
+        outline: none;
+      }
+
+      .demo-buttons {
+        margin: 1em;
+      }
+    </style>
   </div>
-
-  <div class="demo-buttons">
-    <h3>disabled:</h3>
-    <button class="element-primary" disabled>Primary Button</button>
-    <button class="element-success" disabled>Success Button</button>
-    <button class="element-warning" disabled>Warning Button</button>
-    <button class="element-danger" disabled>Danger Button</button>
-    <button class="element-secondary" disabled>Secondary Button</button>
-  </div>
-
-  <style type="text/css">
-     button {
-      padding: 1em;
-      border: none;
-      outline: none;
-    }
-
-    .demo-buttons {
-      margin: 1em;
-    }
-  </style>
-</div>
 `)
 
--- src/examples/content-example.js
+++ src/examples/content-example.js
@@ -1,12 +1,19 @@
-import {append, div, html} from "../util/element";
+import {append, html} from "../util/element";
 
 append(html`
   <div class="content-example">
     <div class="demo-col">
-      <p>You can use the mark tag to <mark>highlight</mark> text.</p>
-      <p><del>This line of text is meant to be treated as deleted text.</del></p>
+      <p>You can use the mark tag to
+        <mark>highlight</mark>
+        text.
+      </p>
+      <p>
+        <del>This line of text is meant to be treated as deleted text.</del>
+      </p>
       <p><s>This line of text is meant to be treated as no longer accurate.</s></p>
-      <p><ins>This line of text is meant to be treated as an addition to the document.</ins></p>
+      <p>
+        <ins>This line of text is meant to be treated as an addition to the document.</ins>
+      </p>
       <p><u>This line of text will render as underlined.</u></p>
       <p><small>This line of text is meant to be treated as fine print.</small></p>
       <p><strong>This line rendered as bold text.</strong></p>
